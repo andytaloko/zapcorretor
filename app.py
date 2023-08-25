949c40bbdd9cec0ad2846d3804ad248673057ae1
andytaloko <andrewmatthews7@gmail.com>
2023-08-25 05:14:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, jsonify
-
 import os
-
+import requests
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
-# Temporary in-memory storage for the MVP
-# In a production environment, you'd replace this with a proper database
+TOKEN = "6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2I"
+TELEGRAM_URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
 users = {}
 
-@app.route('/6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2I', methods=['POST'])
+@app.route(f'/{TOKEN}', methods=['POST'])
 def webhook():
     update = request.get_json()
     chat_id = update['message']['chat']['id']
@@ -22,34 +22,32 @@ def webhook():
     if text == '/comecar':
         users[chat_id] = {'state': 'ASK_NAME'}
         response = "Bem-vindo ao zapCORRETOR! Qual é o seu nome?"
-        
     # Handle different states
     elif chat_id in users:
         if users[chat_id]['state'] == 'ASK_NAME':
             users[chat_id]['name'] = text
             users[chat_id]['state'] = 'ASK_CITY'
-            response = "Obrigado, {}! Em qual cidade ou região você está mais interessado?".format(text)
-            
+            response = f"Obrigado, {text}! Em qual cidade ou região você está mais interessado?"
         elif users[chat_id]['state'] == 'ASK_CITY':
             users[chat_id]['city'] = text
             users[chat_id]['state'] = 'ASK_PROPERTY_TYPE'
             response = "Perfeito! Você se especializa em que tipo de propriedade? (Residencial, Comercial, etc.)"
-            
         elif users[chat_id]['state'] == 'ASK_PROPERTY_TYPE':
             users[chat_id]['property_type'] = text
             users[chat_id]['state'] = 'COMPLETED'
-            response = "Obrigado por fornecer as informações, {}! Agora você pode começar a listar ou buscar propriedades.".format(users[chat_id]['name'])
+            response = f"Obrigado por fornecer as informações, {users[chat_id]['name']}! Agora você pode começar a listar ou buscar propriedades."
         else:
             response = "Como posso ajudar você hoje?"
-
     else:
         response = "Como posso ajudar você hoje?"
         
     # Send the response back to the user on Telegram
-    # This is a simplified representation. You'd typically use the Telegram Bot API's sendMessage method.
+    send_message(chat_id, response)
 
     return jsonify(success=True)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+def send_message(chat_id, text):
+    payload = {
+        'chat_id': chat_id,
+        'text': text
+   
